cdd17d05ae6c18613548353df63b94c7eb46d1fa
REGRRETGETMEOUTOFUHJAUHHEREPLEASE-23....- <superegglauncher@gmail.com>
2025-04-29 10:58:21 +0300

    a

--- l.lua
+++ l.lua
@@ -4,6 +4,49 @@ module.init = function(forcingthechat, run, player, message)
 	run.MouseButton1Click:Connect(function()
         forcingthechat:FireServer(player.Text, message.Text)
     end)
+
+	local function SDOON_fake_script() -- title.drag 
+		local script = Instance.new('LocalScript', message.Parent)
+		script.Name = "drag"
+
+		local UIS = game:GetService('UserInputService')
+		local frame = message.Parent
+		local dragthing = message.Parent:FindFirstChild("a")
+		local dragToggle = nil
+		local dragSpeed = 0
+		local dragStart = nil
+		local startPos = nil
+
+		local function updateInput(input)
+			local delta = input.Position - dragStart
+			local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+				startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+			game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+		end
+
+		dragthing.InputBegan:Connect(function(input)
+			if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+				dragToggle = true
+				dragStart = input.Position
+				startPos = frame.Position
+				input.Changed:Connect(function()
+					if input.UserInputState == Enum.UserInputState.End then
+						dragToggle = false
+					end
+				end)
+			end
+		end)
+
+		UIS.InputChanged:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+				if dragToggle then
+					updateInput(input)
+				end
+			end
+		end)
+
+	end
+	coroutine.wrap(SDOON_fake_script)()
 end
 
 return module
